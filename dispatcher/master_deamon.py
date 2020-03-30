83e198e43b8fde1778b104e37f7109241c5071c6
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@grenoble-inp.org>
2020-03-30 18:29:08 +0200

    Adding waiting for main script and cleaning Dockerfile

--- dispatcher/master_deamon.py
+++ dispatcher/master_deamon.py
@@ -1,14 +1,19 @@
 import os, glob, subprocess
 
-
+processes = []
+proc_dict = {}
 python = 'python'
 script_path = os.path.dirname(os.path.abspath(__file__))
 deamons = glob.glob(os.path.join(script_path, "deamon_*.py"))
 
-print(deamons)
-
 for deamon in deamons:
     proc = subprocess.Popen([python, deamon])
+    processes.append(proc)
+
+
+# Waiting for the completion of the deamons
+for proc in processes:
+    proc.wait()
 
 
 
