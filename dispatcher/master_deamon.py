00b4276553a4475d7d4ecbd648d9bf6f3eabcfc7
Sinitambirivoutin Emrick <emrick.sinitambirivoutin@grenoble-inp.org>
2020-03-30 16:08:14 +0000

    Update master_deamon.py

--- dispatcher/master_deamon.py
+++ dispatcher/master_deamon.py
@@ -1,10 +1,11 @@
 import os, glob, subprocess
 
-processes = []
-proc_dict = {}
+
 python = 'python'
 script_path = os.path.dirname(os.path.abspath(__file__))
 deamons = glob.glob(os.path.join(script_path, "deamon_*.py"))
+
+print(deamons)
 
 for deamon in deamons:
     proc = subprocess.Popen([python, deamon])
